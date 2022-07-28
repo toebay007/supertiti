ff2419c50962627f272e749e74160cd8a12379c3
toebay007 <58075962+toebay007@users.noreply.github.com>
2022-07-29 00:33:06 +0100

    Update connOrders.php

--- connOrders.php
+++ connOrders.php
@@ -4,7 +4,7 @@ class connOrders{
 	protected $conn;			
 	function __construct(){
 		//session_start();				
-		$this->conn = new Mysqli('localhost','root','','supertiti');
+		$this->conn = new Mysqli('heroku_eddc65853faeb30','eu-cdbr-west-03.cleardb.net','1c337c96','bf9d3feb887765');
 	}
 }
 
