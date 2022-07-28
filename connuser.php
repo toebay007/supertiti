407d84b84175e24f1d8be1f1c3bab2230afacd4b
toebay007 <58075962+toebay007@users.noreply.github.com>
2022-07-29 00:36:10 +0100

    Update connuser.php

--- connuser.php
+++ connuser.php
@@ -6,7 +6,7 @@ class connUser
 	function __construct()
 	{
 		session_start();				
-		$this->conn = new Mysqli('localhost','root','','supertiti');
+		$this->conn = new Mysqli('heroku_eddc65853faeb30','eu-cdbr-west-03.cleardb.net','1c337c96','bf9d3feb887765');
 	}
 }
 
