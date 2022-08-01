fda6d506a62997a4fb7a90acae39cd7ae5d82cee
toebay007 <58075962+toebay007@users.noreply.github.com>
2022-08-01 01:54:54 +0100

    Update dbConnect.php

--- dbConnect.php
+++ dbConnect.php
@@ -1,6 +1,6 @@
 <?php
 // Connect with databases
-$mysqli = new mysqli('heroku_eddc65853faeb30','eu-cdbr-west-03.cleardb.net','1c337c96','bf9d3feb887765');
+$mysqli = new mysqli('eu-cdbr-west-03.cleardb.net','bf9d3feb887765','1c337c96','heroku_eddc65853faeb30');
 
 // Display error if failed to connect 
 // Check connection
